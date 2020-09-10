1eaa21933429692936281da77270ed29f29f4217
JeremyC57 <JeremyC57@users.noreply.github.com>
2020-09-09 18:27:26 -0700

    Added Rudimentary Fog/ Enemy Hiding

---
+++ Assets/Scripts/Defog.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Defog : MonoBehaviour
+{
+    [Header("Group of Clouds to be deactivated")]
+    public GameObject[] cloudGroups = new GameObject[1];
+
+    [Header("Enemies and Other Objects to Reveal With this Trigger")]
+    public GameObject[] objectsToReveal = new GameObject[1];
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "playerUnit")
+        {
+            HideClouds();
+            ChangeLayers();
+        }
+    }
+
+    private void ChangeLayers()
+    {
+        foreach( GameObject objectToReveal in objectsToReveal)
+        {
+            objectToReveal.layer = 0;
+        }
+    }
+
+    private void HideClouds()
+    {
+        foreach(GameObject cloudGroup in cloudGroups)
+        {
+            cloudGroup.SetActive(false);
+        }
+    }
+}
