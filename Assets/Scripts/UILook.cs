3ed88c96838d8331a178dc235243a3f2e30e55f7
JeremyC57 <JeremyC57@users.noreply.github.com>
2020-09-10 18:33:55 -0700

    Created UI Bar + Text

---
+++ Assets/Scripts/UILook.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UILook : MonoBehaviour
+{
+    private Transform _mainTransform;
+    private Camera _mainCamera;
+
+    public Image bar;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _mainTransform = transform;
+        _mainCamera = Camera.main;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        _mainTransform.LookAt(_mainCamera.transform.position, Vector3.up);
+    }
+}
