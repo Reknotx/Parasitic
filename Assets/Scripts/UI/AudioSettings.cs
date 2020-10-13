f09fd566e64876ddcbf67716b998ecb062fc0074
JeremyC57 <JeremyC57@users.noreply.github.com>
2020-10-13 15:29:04 -0700

    audio script

---
+++ Assets/Scripts/UI/AudioSettings.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioSettings : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public Slider master, music, sfx;
+
+    [SerializeField]
+    private Text masterText, musicText, sfxText;
+
+
+    private void Start()
+    {
+        LoadLevels();
+        gameObject.SetActive(false);
+    }
+
+
+    public void SetMasterLevel(float sliderValue)
+    {
+        mixer.SetFloat("Master", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("MasterVol", Mathf.Log10(sliderValue) * 20);
+        masterText.text = "Master: " + Mathf.Round(sliderValue * 100);
+
+    }
+
+    public void SetMusicLevel(float sliderValue)
+    {
+        mixer.SetFloat("Music", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+        musicText.text = "Music: " + Mathf.Round(sliderValue * 100);
+
+    }
+
+    public void SetSFXLevel(float sliderValue)
+    {
+        mixer.SetFloat("SFX", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("SFXVol", Mathf.Log10(sliderValue) * 20);
+        sfxText.text = "SFX: " + Mathf.Round(sliderValue * 100);
+    }
+
+    private void LoadLevels()
+    {
+        mixer.GetFloat("Master", out float mastValue);
+        mixer.GetFloat("Music", out float musicValue);
+        mixer.GetFloat("SFX", out float sfxValue);
+        if (PlayerPrefs.HasKey("MasterVol"))
+        {
+            master.value = Mathf.Pow(10f, (PlayerPrefs.GetFloat("MasterVol") / 20));
+            mixer.SetFloat("Master", PlayerPrefs.GetFloat("MasterVol"));
+        }
+        else
+        {
+            PlayerPrefs.SetFloat("MasterVol", mastValue);
+            master.value = Mathf.Pow(10f, (mastValue / 20));
+        }
+
+        if (PlayerPrefs.HasKey("MusicVol"))
+        {
+            music.value = Mathf.Pow(10f, (PlayerPrefs.GetFloat("MusicVol") / 20));
+            mixer.SetFloat("Music", PlayerPrefs.GetFloat("MusicVol"));
+        }
+        else
+        {
+            PlayerPrefs.SetFloat("MusicVol", musicValue);
+            music.value = Mathf.Pow(10f, (musicValue / 20));
+        }
+
+        if (PlayerPrefs.HasKey("SFXVol"))
+        {
+            sfx.value = Mathf.Pow(10f, (PlayerPrefs.GetFloat("SFXVol") / 20));
+            mixer.SetFloat("SFX", PlayerPrefs.GetFloat("SFXVol"));
+        }
+        else
+        {
+            PlayerPrefs.SetFloat("SFXVol", sfxValue);
+            sfx.value = Mathf.Pow(10f, (sfxValue / 20));
+        }
+
+        masterText.text = "Master: " + Mathf.Round(master.value * 100);
+        musicText.text = "Music: " + Mathf.Round(music.value * 100);
+        sfxText.text = "SFX: " + Mathf.Round(sfx.value * 100);
+    }
+}
