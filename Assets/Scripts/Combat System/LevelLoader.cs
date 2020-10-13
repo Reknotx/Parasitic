43e534d26ad502a7cd5ae9ea525fe0d627966420
Reknotx <chaseoconnor3@comcast.net>
2020-10-13 16:49:50 -0700

    Transitions work

---
+++ Assets/Scripts/Combat System/LevelLoader.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CombatSystem.Instance.state == BattleState.Won)
+        {
+            LoadNextLevel();
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        //Play the animation
+        transition.SetTrigger("FadeOut");
+
+        //Wait
+        yield return new WaitForSeconds(1);
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync(levelIndex);
+
+        while (!operation.isDone)
+        {
+            yield return null;
+        }
+    }
+
+}
