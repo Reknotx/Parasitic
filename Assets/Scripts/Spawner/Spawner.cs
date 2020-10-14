bf26e63d8e77f1b09199154dba3a20521cb96daa
AFriendlyDanger <60414692+AFriendlyDanger@users.noreply.github.com>
2020-10-14 01:43:11 -0700

    Spawner Script

---
+++ Assets/Scripts/Spawner/Spawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject spawnee;
+    public int maxSpawns = 1;
+    int currentSpawns = 0;
+
+    public void SpawnUnit(Tile tile)
+    {
+        if (currentSpawns < maxSpawns && !tile.occupied)
+        {
+            Instantiate(spawnee, tile.transform.position + Vector3.up, Quaternion.identity);
+            currentSpawns++;
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner/TriggerSpawn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerSpawn : MonoBehaviour
+{
+    Spawner spawner;
+    Tile tile;
+    private void Start()
+    {
+        spawner = GetComponent<Spawner>();
+        tile = MapGrid.Instance.TileFromPosition(transform.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("trigger entered");
+        if (other.CompareTag("Player"))
+        {
+            spawner.SpawnUnit(tile);
+        }
+    }
+}
