586dfcf1c8232dce0e648e47d1adbec4b01b471c
AFriendlyDanger <60414692+AFriendlyDanger@users.noreply.github.com>
2020-10-15 16:58:19 -0700

    level 1

---
+++ Assets/Scripts/Spawner/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject spawnee;
+    public int maxSpawns = 1;
+    int currentSpawns = 0;
+
+    public void SpawnUnit(Tile tile)
+    {
+        if (currentSpawns < maxSpawns && !tile.occupied)
+        {
+            GameObject spawnedObject = Instantiate(spawnee, tile.transform.position + Vector3.up * spawnee.transform.position.y, Quaternion.identity);
+            currentSpawns++;
+            Enemy unit = spawnedObject.GetComponent<Enemy>();
+            if (unit)
+            {
+                CombatSystem.Instance.SubscribeEnemy(unit);
+                //ask chase about subscribe timer
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner/TriggerSpawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerSpawn : MonoBehaviour
+{
+    Spawner spawner;
+    Tile tile;
+    public Transform spawnPoint;
+    private void Start()
+    {
+       
+        spawner = transform.GetComponentInParent<Spawner>();
+        tile = MapGrid.Instance.TileFromPosition(spawnPoint.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("trigger entered");
+        if (other.CompareTag("Player"))
+        {
+            spawner.SpawnUnit(tile);
+        }
+    }
+}
