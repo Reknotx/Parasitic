bd9d32d1f0f3ff33055dad404347fb857d944807
AFriendlyDanger <60414692+AFriendlyDanger@users.noreply.github.com>
2020-10-15 02:41:54 -0700

    SpawnScript

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject Spawnee;
+    public int maxSpawns = 5;
+    public int currentSpawns = 0;
+    public bool spawning = false;
+
+    //what is the range of the spawner?
+    //what causes the spawner to start spawning?
+    //how often does a spawner spawn an enemy?
+    //spawnpoint
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
