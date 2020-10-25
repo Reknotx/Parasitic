2bd6c232a604d38dc96173bf0400f478254cc100
Chase O'Connor <chaseoconnor3@comcast.net>
2020-10-24 18:06:21 -0700

    The pre-planning phase
    throwing stuff at a wall, let's see what sticks

--- Assets/Scripts/Unit Scripts/Enemies/Spiker.cs
+++ Assets/Scripts/Unit Scripts/Enemies/Spiker.cs
@@ -18,6 +18,85 @@ public class Spiker : Enemy
     }
     #endregion
 
-    
+    public override void Move(List<Tile> path)
+    {
+        if (CheckIfInRangeOfTarget() == false)
+        {
+            base.Move(path);
+        }
+        else
+        {
+            // Perform the unique spiker functions.
+        }
+    }
+
+    enum CloserIn { X, Y }
+
+    public void FindOptimalMaxRangePos()
+    {
+        if (_currTarget == null)
+        {
+            FindNearestPlayer();
+        }
+
+        ///Perhaps find which direction is closer to the player?
+        ///Like if we are closer in x and farther away in the Y then
+        ///move away from the player in the X direction. 
+        ///Vice versa for a situation when we are closer in the Y but farther 
+        ///in the X
+        ///
+        Coord playerCoord = new Coord(0, 0);
+        Coord spikerCoord = new Coord(0, 0);
+
+        playerCoord.X = (int)_currTarget.currentTile.gridPosition.x;
+        playerCoord.Y = (int)_currTarget.currentTile.gridPosition.y;
+
+        spikerCoord.X = (int)currentTile.gridPosition.x;
+        spikerCoord.Y = (int)currentTile.gridPosition.y;
+
+        int xDelta = Mathf.Abs(playerCoord.X - spikerCoord.X);
+        int yDelta = Mathf.Abs(playerCoord.Y - spikerCoord.Y);
+
+
+
+    }
+
+    void Notes()
+    {
+        /*
+         * 1. If the spiker is not in range then he needs to move towards
+         * a player until he is in range. This is already dealt with with 
+         * the normal behaviour of the parent scripts.
+         * 
+         * 2. If Spiker is within range, but not at MAXIMUM range from the
+         * closest player, then the spiker needs to move until they are at 
+         * max range from the player
+         * 
+         * 3. We can just move in any direction that keeps us within range
+         * and moving away from the player, depending on current X and Y
+         * position on the grid.
+         * 
+         * 
+         * 
+         */
+
+    }
+    //public override void Dodge()
+    //{
+    //    Debug.Log("Spiker Dodge");
+    //    base.Dodge();
+    //}
+
+    private class Coord
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Coord(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
 
 }
