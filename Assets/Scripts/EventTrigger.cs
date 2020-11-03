81ca0ecdfc479c3ebcd11fed84c78ca28eb1da39
JeremyC57 <JeremyC57@users.noreply.github.com>
2020-11-03 15:13:08 -0800

    added fog tutorial stuff and feedback button in pause

--- Assets/Scripts/EventTrigger.cs
+++ Assets/Scripts/EventTrigger.cs
@@ -10,7 +10,6 @@ public enum eventType
 }
 
 
-
 public class EventTrigger : MonoBehaviour
 {
     
@@ -20,7 +19,63 @@ public class EventTrigger : MonoBehaviour
 
     public UnitToUpgrade unit;
 
+    public int pointsToAdd = 0;
+
     public GameObject spawnedObject;
 
-    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            switch (type)
+            {
+                case eventType.addUpgradePoints:
+                    addPoints(unit);
+                    break;
+                case eventType.spawnTutoiralScreen:
+                    spawnedObject.SetActive(true);
+                    break;
+                case eventType.none:
+                    break;
+                default:
+                    break;
+            }
+
+            switch (subType)
+            {
+                case eventType.addUpgradePoints:
+                    addPoints(unit);
+                    break;
+                case eventType.spawnTutoiralScreen:
+                    spawnedObject.SetActive(true);
+                    break;
+                case eventType.none:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+
+    private void addPoints(UnitToUpgrade unit)
+    {
+        switch (unit)
+        {
+            case UnitToUpgrade.mage:
+                Upgrades.Instance.MagePoints += pointsToAdd;
+                break;
+            case UnitToUpgrade.knight:
+                Upgrades.Instance.KnightPoints += pointsToAdd;
+                break;
+            case UnitToUpgrade.archer:
+                Upgrades.Instance.ArcherPoints += pointsToAdd;
+                break;
+            case UnitToUpgrade.none:
+                break;
+            default:
+                break;
+        }
+    }
 }
