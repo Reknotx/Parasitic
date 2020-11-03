38f595c37884c846a004c4a2ae560d6f02956f95
AFriendlyDanger <60414692+AFriendlyDanger@users.noreply.github.com>
2020-11-02 18:02:08 -0800

    key

---
+++ Assets/Scripts/Map/Key.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    public List<Tile> tilesToOpen;
+    public List<GameObject> objectsToHide;
+    public List<GameObject> objectsToShow;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            foreach(Tile tile in tilesToOpen)
+            {
+                tile.movementTile = true;
+                tile.blocksLOS = false;
+            }
+            foreach(GameObject item in objectsToHide)
+            {
+                item.SetActive(false);
+            }
+            foreach (GameObject item in objectsToShow)
+            {
+                item.SetActive(true);
+            }
+            gameObject.SetActive(false);
+        }
+    }
+}
